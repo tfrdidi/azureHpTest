ddc6f3290944b2d04a958238d0c732d3235b3e5a
Lukas Hahmann <tfr.didi@gmail.com>
2017-10-20 20:53:32 +0200

    Style in less umgebaut

---
+++ hp-test/src/app/model.ts
@@ -0,0 +1,11 @@
+export class Navigation {
+    public title : string;
+
+    public pages : Array<Page>;
+}
+
+export class Page {
+    public title : string;
+    public subtitle : string;
+    public image : string;
+}
